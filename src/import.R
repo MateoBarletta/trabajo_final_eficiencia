6973be19cab16207fd2ab74fd27e5c12c22c0aad
Nano <mateobarletta@gmail.com>
2020-01-02 14:56:22 -0300

    archivo import

---
+++ src/import.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(here)
+library(readxl)
+library(janitor)
+library(writexl)
+
+
+path <- here::here("trabajo_final", "data", "eaae")
+
+col1 <- c("seccion",	"division", "descripcion", "VBP", "CI", "VAB", "REM", "IMP","CKF", "EEN")
+col2 <- c("seccion",	"division", "descripcion", "VBP", "CI", "IMP", "CKF", "VAB","REM", "EEN")
+
+df_2016 <- read_excel(file.path(path, "EAE_C2.5_2016.xls"),
+                      skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2016')
+
+df_2015 <- read_excel(file.path(path, "EAE_C2.6_2015.xls"),
+                                 skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2015')
+
+df_2014 <- read_excel(file.path(path, "EAE_C2.6_2014.xls"),
+                      skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2014')
+
+df_2013 <- read_excel(file.path(path, "EAE_C2.6_2013.xls"),
+                      skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2013')
+
+df_2012 <- read_excel(file.path(path, "EAE_C2.6_2012.xls"),
+                      skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2012')
+
+df_2011 <- read_excel(file.path(path, "EAE_C2_2011.xls"),
+                      skip = 11, col_names = col1) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2011')
+
+df_2010 <- read_excel(file.path(path, "EAE_C2_2010.xlsx"),
+                      skip = 11, col_names = col2) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2010')
+
+df_2009 <- read_excel(file.path(path, "EAE_C2_2009.xlsx"),
+                      skip = 11, col_names = col2) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2009')
+
+df_2008 <- read_excel(file.path(path, "EAE_C2_2008.xls"),
+                      skip = 11, col_names = col2) %>% 
+  filter(!is.na(VBP)) %>% 
+  mutate(anio = '2008')
+
+
+df_eaae <- bind_rows(df_2008, df_2009, df_2010, df_2011, df_2012, df_2013, df_2014, df_2015, df_2016) %>% 
+  clean_names() %>% 
+  filter(seccion == "C") %>% 
+  select(anio, seccion, division, descripcion, vbp, vab, ci, rem, ckf, everything())
+
+df_eaae %>% 
+  select("vbp", "ci", "imp", "ckf", "vab","rem", "een") %>% 
+  cor()
+
+#write_xlsx(df_eaae, "trabajo_final/data/eaae.xlsx")
+
+
+rm(df_2008, df_2009, df_2010, df_2011, df_2012, df_2013, df_2014, df_2015, df_2016)
+rm(path, col1, col2)
+  
